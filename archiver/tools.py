eb1e1f9be5d075d90f5af8105272b7dce0c55f41
nicourrrn <s57111702@gmail.com>
2023-10-14 18:03:04 +0300

    With filename bug

--- archiver/tools.py
+++ archiver/tools.py
@@ -2,6 +2,7 @@ import shutil
 from datetime import datetime
 import logging
 import asyncio
+from random import randint as rint
 
 from . import config
 
@@ -10,12 +11,10 @@ async def timered_maker(timer: config.Timer):
     while True:
         await asyncio.sleep(timer.seconds)
         logging.info(f"Run with timer {timer.seconds}")
-
-        archname = "archive_" + str(datetime.today())[:-7].replace(" ", "_") \
-                .replace(":", "").replace("-", "") + ".zip"
+        now = datetime.today()
+        archname = f"archive_{now.strftime('%Y%m%d%H%S')}{rint(1000, 9999)}"
+        archname = f"archive_{now.strftime('%Y%m%d%H%S')}"
         logging.info(f"Make an archive with name {timer.filename}")
-
         shutil.make_archive(archname, "zip", timer.filename)
-
         logging.info(f"Done with timer {timer.seconds}")
 
